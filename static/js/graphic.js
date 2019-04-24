c406fd1fd5fe4454a3c1adbfea95b188ee290d65
Oliveira <eliaspradoprofessional@outlook.com>
2019-04-24 01:07:44 +0000

    added boilier plate code and designed first barchart with dc.barchart()

--- static/js/graphic.js
+++ static/js/graphic.js
@@ -6,9 +6,24 @@ function makeGraphs(error, salaryData){
     var ndx = crossfilter(salaryData);
     
     show_gender_balance(ndx);// will be created on next line..
+    
+    dc.renderAll();
 }
 
 function show_gender_balance(ndx){
     var dim = ndx.dimension(dc.pluck('sex'));
     var group = dim.group();
+    
+    dc.barChart("#gender-balance")
+    .width(400)
+    .height(300)
+    .margins({top:10, right:50, bottom:30, left:50})
+    .dimension(dim)// var dim declared within this function
+    .group(group)// var group declared within this function
+    .transitionDuration(500)//tells how quicly the chart will be animated 
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .elasticY(true)
+    .xAxisLabel('Gender')
+    .yAxis().ticks(20);
 }
