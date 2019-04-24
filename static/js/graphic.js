199b93999fa634fbb2ec408b6f82f13e39fd56c2
Oliveira <eliaspradoprofessional@outlook.com>
2019-04-24 01:14:29 +0000

    fixed comments

--- static/js/graphic.js
+++ static/js/graphic.js
@@ -1,11 +1,11 @@
 queue()//use the queue library to load data
-.defer(d3.csv, "data/Salaries.csv")// defer will take the format in which will be loaded the data as well as its path. two arguments 1st: the format and 2nd the path It could be a d3.json!
+.defer(d3.csv, "data/Salaries.csv")// defer will take the format in which will be loaded the data (csv or json) as well as its path. two arguments 1st: the format and 2nd the path.
 .await(makeGraphs);// the await method takes the name of the function that we want to call when the data has been downloaded. 
 
 function makeGraphs(error, salaryData){
     var ndx = crossfilter(salaryData);
     
-    show_gender_balance(ndx);// will be created on next line..
+    show_gender_balance(ndx);// will be declared on the show_gender_balance function..
     
     dc.renderAll();
 }
